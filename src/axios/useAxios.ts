1ce422641a236cf0b665d4c204750a7f33f9edf7
The-FerryMan1 <mercedroxy@gmail.com>
2025-10-11 12:20:44 +0800

    add domain name

--- src/axios/useAxios.ts
+++ src/axios/useAxios.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 export const useAxios = axios.create({
-    baseURL: 'http://localhost:8888/api/', 
+  baseURL: import.meta.env.VITE_DOMAIN_NAMe,
 
-
-    // productions
-    // baseURL: 'https://vitapulse-api.onrender.com/api/', 
-    withCredentials: true
+  // productions
+  // baseURL: 'https://vitapulse-api.onrender.com/api/',
+  withCredentials: true,
 });
